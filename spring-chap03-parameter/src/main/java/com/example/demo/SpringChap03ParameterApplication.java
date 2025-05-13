0a69c5fa5ea2f30a6d53fb41f75842418f65253b
aaa <aaa@aaa.com>
2025-05-13 17:42:55 +0900

    o

---
+++ spring-chap03-parameter/src/main/java/com/example/demo/SpringChap03ParameterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChap03ParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChap03ParameterApplication.class, args);
+	}
+
+}
